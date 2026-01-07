0be23e4153d8312aa1225bfec7a1a27576a227fe
Darius Ronkowitz <dronkowitz@yahoo.com>
2026-01-07 08:53:35 -0500

    `Updated TeamSetup.cs`
    Added RequireComponent to the script; this is necessary to ensure the script works correctly.

--- TeamSetup.cs
+++ TeamSetup.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterSwitch))]
 public class TeamSetup : MonoBehaviour
 {
     public TeamComposition CurrentTeam;
