9655ed43fec9434c875a27fb3730c3998f86c7dd
Darius Ronkowitz <dronkowitz@yahoo.com>
2026-01-25 20:49:26 -0500

    Add files via upload

--- Frog.cs
+++ Frog.cs
@@ -1,37 +1,12 @@
 using UnityEngine;
 using UnityEngine.AI;
-[RequireComponent(typeof(Rigidbody))]
-[RequireComponent(typeof(Animator))]
-[RequireComponent(typeof(Health))]
+
+
 public class Frog : MonoBehaviour
 {
-    public Transform player;
-    public Health frogHealth;
-    public NavMeshAgent agent;
-    public Vector3 origin;
-    public float croakTimer = 0f;
-    public AudioClip croakSoundTime = null;
-    private Animator anim;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        origin = transform.position;
-        player = GameObject.FindWithTag("Player").transform;
-        anim = GetComponent<Animator>();
-        anim.SetBool("Croak", true);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void PlayCroakSound()
-    {
-        if (croakSoundTime != null)
-        {
-            player.GetComponent("Croak");
-        }
-    }
+    // This script controls the frog's movement using Unity's NavMeshAgent.
+    // The frog is only avable in the stages 09 and 10.
+    // Add The Black Frog and green frog prefabs to the scene with this script attached.
+    
+   
 }
