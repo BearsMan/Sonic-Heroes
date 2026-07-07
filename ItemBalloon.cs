22bbef46be1f20f78bd5b3a8377e48b9ca8c9bc7
Darius Ronkowitz <dronkowitz@yahoo.com>
2026-07-07 08:38:51 -0400

    Refactor ItemBalloon and add LevelUp functionality
    Refactored ItemBalloon class and added LevelUp class with functionality for player level-up events.

--- ItemBalloon.cs
+++ ItemBalloon.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemBalloon : MonoBehaviour
+public class LevelUp : MonoBehaviour
 {
-    
-    public GameObject[] pickables;
-    public bool popped = false;
+    public GameObject levelUpHUD;
+    public GameObject levelUpParent;
+    public GameObject levelUpPrefab;
+    public List<AudioClip> characterSFX = new List<AudioClip>();
+    public bool levelingup;
+    public float speedCoreLevelUp;
+    public float powerCoreLevelUp;
+    public float flyCoreLevelUp;
+    public bool isSpeed, isFlying, isPower;
+    public Sprite bluePowerCore, redPowerCore, yellowPowerCore;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,16 +25,55 @@ public class ItemBalloon : MonoBehaviour
     {
         
     }
-    private void OnTriggerEnter(Collider other)
+    public void LevelUpSound()
     {
-        if (other.gameObject.CompareTag("Player") && popped == false)
+        GetComponent<AudioSource>().Play();
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && levelingup == false)
         {
-            int pick = Random.Range(0, pickables.Length);
-            Instantiate(pickables[pick], other.transform.position,Quaternion.identity);
-            GetComponent<AudioSource>().Play();
-            Object.FindAnyObjectByType<HUD>().AddPower(1);
-            popped = true;
-            Destroy(gameObject);
+            levelingup = true;
+            GetComponent<SpriteRenderer>().enabled = false;
+            GameInstance.speedLevelUp += 1;
+            GameInstance.flyLevelUp += 1;
+            GameInstance.powerLevelUp += 1;
+            Instantiate(levelUpPrefab, levelUpParent.transform);
+            StartCoroutine(PlayLevelUpSFX());
+            Object.FindAnyObjectByType<HUD>().AddPower(5);
         }
     }
+    
+    public IEnumerator PlayLevelUpSFX()
+    {
+        GetComponent<AudioSource>().Play();
+        while (GetComponent<AudioSource>().isPlaying == true)
+        {
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        GetComponent<AudioSource>().clip = characterSFX[GameInstance.currentTeam];
+        GetComponent<AudioSource>().Play();
+
+        while (GetComponent<AudioSource>().isPlaying == true)
+        {
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        Destroy(gameObject);
+    }
+    public IEnumerator LevelUpCharacter(GameObject speedCharacter)
+    {
+        speedCharacter.GetComponent<UltimatePlayerMovement>().leftFollower.SetActive(true);
+        speedCharacter.GetComponent<UltimatePlayerMovement>().rightFollower.SetActive(true);
+        
+
+        yield return null;
+    }
+
+    //This is the team selections setup from LevelUpCore.cs for Character Level Up Sound Effects to be played in correct team order
+    //Team Sonic = 0
+    //Team Dark = 1
+    //Team Rose = 2
+    //Team Chaotix = 3
+    
+   
 }
