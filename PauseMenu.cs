a51386ef8659eb1f86aeb9b600545b4949c5d607
Darius Ronkowitz <dronkowitz@yahoo.com>
2026-07-07 08:55:52 -0400

    Refactor PauseMenu to use FindObjectsByType

--- PauseMenu.cs
+++ PauseMenu.cs
@@ -8,7 +8,90 @@ public class PauseMenu : MonoBehaviour
     public GameObject pauseMenu;
     public List<AudioSource> pausedSounds;
     
+    // Start is called before the first frame updateusing System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public List<AudioSource> pausedSounds;
+
     // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SwitchPause();
+        }
+    }
+
+    public void PauseGame()
+    {
+        pausedSounds = new List<AudioSource>();
+        AudioSource[] allSounds = Object.FindObjectsByType<AudioSource>(FindObjectsInactive.Include);
+        foreach (AudioSource source in allSounds)
+        {
+            if (source.isPlaying)
+            {
+                source.Pause();
+                pausedSounds.Add(source);
+            }
+        }
+
+        foreach (TriggerDialog td in Object.FindObjectsByType<TriggerDialog>(FindObjectsInactive.Include))
+        {
+            td.pause = true;
+        }
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void UnpauseGame()
+    {
+        foreach (AudioSource source in pausedSounds)
+        {
+            source.UnPause();
+        }
+        foreach (TriggerDialog td in Object.FindObjectsByType<TriggerDialog>(FindObjectsInactive.Include))
+        {
+            td.pause = false;
+        }
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+
+    public void SwitchPause()
+    {
+        if (pauseMenu.activeSelf)
+        {
+            UnpauseGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+
+    }
+
+    public void Restartlevel()
+    {
+        Scene myscene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(myscene.name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
     void Start()
     {
         
