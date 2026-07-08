1d14a7411e64e07f5576666b197908dadc4c9c05
Darius Ronkowitz <dronkowitz@yahoo.com>
2026-07-08 16:15:08 -0400

    `Updated Crate.cs`
    Improved Crate Sound Code.

--- Crate.cs
+++ Crate.cs
@@ -1,20 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Crate : MonoBehaviour
 {
     public GameObject[] spawnable = new GameObject[0];
     public AudioClip breakSound;
+    private bool HasPlayedCrateSound = false;
     public void DestroyCrate()
     {
+        if (HasPlayedCrateSound)
+        {
+            return;
+        }
+        HasPlayedCrateSound = true;
+
         int num = Random.Range(0, spawnable.Length);
         Instantiate(spawnable[num], transform.position, transform.rotation);
 
-        GameObject source = Resources.Load<GameObject>("Audio Object");
-        source = Instantiate(source, transform.position, transform.rotation);
-        if (source != null && source.TryGetComponent<AudioObject>(out var srcAo)) srcAo.Setup(breakSound, transform);
-        Destroy(gameObject);//Replace with broken crate model
+        GameObject source = Resources.Load<GameObject>("AudioObject");
+        if (source != null)
+        {
+            source = Instantiate(source, transform.position, transform.rotation);
+
+            if (source.TryGetComponent<AudioObject>(out var srcObj))
+            {
+                srcObj.Setup(breakSound, transform);
+            }
+        }
+
+        Destroy(gameObject);
     }
 
     private void OnCollisionEnter(Collision collision)
