1e1e10245a34838d9f1b3cb883de58ad54b47dd5
Darius Ronkowitz <dronkowitz@yahoo.com>
2026-07-13 16:20:19 -0400

    `Updated PickUpObject.cs`
    Fixed bugs with picking up objects in the game world; this now applies to all 3D GameObjects, not just one object per.

--- PickUpObject.cs
+++ PickUpObject.cs
@@ -1,19 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Collider))]
 public class PickUpObject : MonoBehaviour
 {
+    private bool collected = false;
     public int ringValue = 0;
     public int powerValue = 0;
     public GameObject vis;
     public Sprite itemSprite;
-    
+
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.TryGetComponent(out UltimatePlayerMovement player))
+        Debug.LogWarning("PickUpObject: OnTriggerEnter called with " + other.name);
+        if (collected)
         {
-            AddEffect();
+            return;
+        }
+
+        UltimatePlayerMovement playerMovement = other.GetComponentInParent<UltimatePlayerMovement>();
+        FollowerNavigation followerNavigation = other.GetComponentInParent<FollowerNavigation>();
+        Debug.LogWarning("PickUpObject: playerMovement is " + (playerMovement != null ? "not null" : "null") + ", followerNavigation is " + (followerNavigation != null ? "not null" : "null"));
+        CharacterType teamCharacters = null;
+
+        if (playerMovement != null && playerMovement.currentCharacter != null)
+        {
+            teamCharacters = playerMovement.currentCharacter.GetComponent<CharacterType>();
+
+        }
+        else if (followerNavigation != null)
+        {
+            teamCharacters = followerNavigation.GetComponentInChildren<CharacterType>();
+        }
+            Debug.LogWarning("PickUpObject: teamCharacters is " + (teamCharacters != null ? "not null" : "null"));
+
+        if (teamCharacters != null)
+        {
+            collected = true;
+            AddEffect(teamCharacters.type);
         }
     }
 
@@ -22,23 +46,28 @@ public class PickUpObject : MonoBehaviour
         transform.Rotate(Vector3.up, 90 * Time.deltaTime);
     }
 
-    protected virtual void AddEffect()
+    protected virtual void AddEffect(CHARACTERTYPES characterTypes)
     {
-        // Look up HUD explicitly and only interact with it if present to avoid null references.
-        HUD hud = Object.FindAnyObjectByType<HUD>();
+        HUD hud = FindAnyObjectByType<HUD>();
         if (hud != null)
         {
             hud.AddPower(powerValue);
-            hud.ShowPickUp(itemSprite);
         }
 
-        // Always apply game state changes and visuals/audio regardless of HUD presence.
-        GameInstance.currentRings += ringValue;
-        vis.SetActive(false);
-        GetComponent<Collider>().enabled = false;
-        AudioSource source = GetComponent<AudioSource>();
-        source.Play();
-    }
+        if (ringValue > 0)
+        {
+           GameInstance.AddRings(characterTypes, ringValue);
+        }
 
-    
+        vis.SetActive(false);
+        if (TryGetComponent<Collider>(out var pickUpItem))
+        {
+            pickUpItem.enabled = false;
+        }
+
+        if (TryGetComponent<AudioSource>(out var pickupSource))
+        {
+            pickupSource.Play();
+        }
+    }
 }
