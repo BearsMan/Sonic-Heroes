39b74d1b9c4099052def3547a8a14d0e1c1185c9
Darius Ronkowitz <dronkowitz@yahoo.com>
2026-07-23 20:26:57 -0400

    `Updated Goal Codes`
    Added a way to set up special stage completed, etc.

--- GoalRing.cs
+++ GoalRing.cs
@@ -7,6 +7,7 @@ public class GoalRing : MonoBehaviour
     public GameObject LevelEndHUD;
     public string NextLevelName;
     public string specialStageSceneName;
+    [SerializeField] private bool isSpecialStageGoal = false;
     private bool LoadingNextScene = false;
     public AudioClip clip;
     private GameObject source;
@@ -27,15 +28,40 @@ public class GoalRing : MonoBehaviour
         if (other.TryGetComponent(out UltimatePlayerMovement player) && LoadingNextScene == false)
         {
             LoadingNextScene = true;
+
             GameObject ao = Instantiate(source, transform.position, Quaternion.identity);
             ao.GetComponent<AudioObject>().Setup(clip, transform);
-            GameInstance.CreateSave();
+
+            if (!isSpecialStageGoal)
+            {
+                GameInstance.CreateSave();
+            }
+
             StartCoroutine(EndOfLevel());
         }
     }
 
     public IEnumerator EndOfLevel()
     {
+        if (isSpecialStageGoal)
+        {
+            // Future celebration:
+            // - Play "Special Stage Clear!"
+            // - Show Chaos Emerald
+            // - Play victory music
+            // - Show score bonus
+
+            yield return new WaitForSeconds(2f);
+
+            SpecialStageManager manager = FindAnyObjectByType<SpecialStageManager>();
+
+            if (manager != null)
+            {
+                manager.CompleteStage();
+            }
+
+            yield break;
+        }
         ScoreSystem sys = Object.FindAnyObjectByType<ScoreSystem>();
 
         if (sys != null)
@@ -51,6 +77,7 @@ public class GoalRing : MonoBehaviour
             GameInstance.returnScene = NextLevelName;
             SceneManager.LoadScene(specialStageSceneName);
         }
+
         else
         {
             SceneManager.LoadScene(NextLevelName);
