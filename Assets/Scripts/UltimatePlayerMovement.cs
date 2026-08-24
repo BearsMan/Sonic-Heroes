e04757ea84bf3456fa7a1d40e801806da89596c6
Darius Ronkowitz <dronk14@hotmail.com>
2026-08-23 20:27:38 -0400

    Added Rigidbody requirement.

--- Assets/Scripts/UltimatePlayerMovement.cs
+++ Assets/Scripts/UltimatePlayerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class UltimatePlayerMovement : MonoBehaviour
 {
     #region constant variables
